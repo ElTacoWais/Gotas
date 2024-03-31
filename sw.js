f81fcd7139cb20ac5af4b48097a4ba34cf9dcd39
ElTacoWais <146405980+ElTacoWais@users.noreply.github.com>
2024-03-31 01:33:22 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,60 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('gotas-cache').then(function(cache) {
-            return cache.addAll([
-                '/',
-                'index.html',
-                'styles.css',
-                'script.js',
-                'alarma.mp3'
-            ]);
-        })
-    );
+// Asignar un nombre y versión al cache
+const CACHE_NAME = 'cache_lista';
+const urlsToCache = [
+  './',
+  './index.html',
+  './styles.css',
+  './script.js',
+  './icono1.png',
+  './icono2.png',
+  './alarma.mp3'
+];
+
+// Durante la fase de instalación, generalmente se almacena en caché los activos estáticos
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache)
+          .then(() => self.skipWaiting());
+      })
+      .catch(err => console.log('Falló registro de cache', err))
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
+// Una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
+self.addEventListener('activate', e => {
+  const cacheWhitelist = [CACHE_NAME];
+
+  e.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            // Eliminamos lo que ya no se necesita en cache
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+      // Le indica al SW activar el cache actual
+      .then(() => self.clients.claim())
+  );
+});
+
+// Cuando el navegador recupera una url
+self.addEventListener('fetch', e => {
+  // Responder ya sea con el objeto en caché o continuar y buscar la url real
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => {
+        if (res) {
+          // Recuperar del cache
+          return res;
+        }
+        // Recuperar de la petición a la url
+        return fetch(e.request);
+      })
+  );
 });
